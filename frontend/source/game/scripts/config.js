57a678b8b65b83669cb3a3a38ba7de0df0d106ab
Nikita Stroganov <findoss@yandex.ru>
2017-05-14 14:27:46 +0300

    инициализация игры

--- frontend/source/game/scripts/config.js
+++ frontend/source/game/scripts/config.js
@@ -1,1 +1,9 @@
-
+var config = {
+  width :  1920,
+  height : 1080,
+  renderer : Phaser.AUTO,
+  antialias : true,
+  multiTexture : true,
+  transparent : true,
+  enableDebug : true
+}
---
+++ frontend/source/game/scripts/test.js
@@ -0,0 +1,7 @@
+var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
+//game.state.add('Boot', Boot);
+//game.state.add('Preloader', Preloader);
+//game.state.add('MainMenu', MainMenu);
+game.state.add("PlayGame", PlayGame);
+
+game.state.start("PlayGame");
