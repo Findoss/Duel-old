96d5e59d03e475bf30aade1ac5f778c8b45f2fe5
Nikita Stroganov <findoss@yandex.ru>
2017-05-14 14:40:39 +0300

    переименование стартового скрипта

--- frontend/source/game/scripts/main.js
+++ frontend/source/game/scripts/main.js
@@ -4,4 +4,4 @@ var game = new Phaser.Game(1920, 1080, Phaser.AUTO);
 //game.state.add('MainMenu', MainMenu);
 game.state.add("PlayGame", PlayGame);
 
-game.state.start("PlayGame");
+game.state.start("PlayGame", true, false, {param:true});
