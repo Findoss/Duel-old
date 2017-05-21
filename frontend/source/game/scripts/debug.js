2fe217cfe42eb3b10518a80751d46e863426f83c
Nikita Stroganov <findoss@yandex.ru>
2017-05-21 22:01:09 +0300

    убран обработчик событий

--- frontend/source/game/scripts/debug.js
+++ frontend/source/game/scripts/debug.js
@@ -1,4 +1,15 @@
 var DEBUG = true;
-
 var DEBUG_font = "30px Arial";
 var DEBUG_color = "#00ff00";
+
+/*
+q.onNewType.add(function(x) {
+  game.add.text(
+    100, 
+    100, 
+    x.row+"x"+x.col, {
+    font: "30px Arial",
+    fill: "#000",
+    align: "center" 
+  });
+},this);*/
