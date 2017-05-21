617d8f0786c8418b6db7d70f14c103fde0a2bdc1
Nikita Stroganov <findoss@yandex.ru>
2017-05-21 22:02:44 +0300

    добавление текстуры руны

---
+++ frontend/source/game/scripts/textures.js
@@ -0,0 +1,15 @@
+var path = "./images/";
+
+var textureRune = {
+  fileName: path+"rune_",
+  size: {
+    width :  100,
+    height : 100
+  },
+  animations: {
+    wait: [0, 1, 2, 3],
+    focus: [4],
+    pick: [5, 6, 7],
+    destroy: [8, 9, 10, 11]
+  }
+}
