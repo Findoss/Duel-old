a70f9cb16e22393be1f24a7d801ae609eacfa3d7
Nikita Stroganov <findoss@yandex.ru>
2017-05-21 22:02:14 +0300

    переименование объекта игры

--- frontend/source/game/scripts/main.js
+++ frontend/source/game/scripts/main.js
@@ -2,8 +2,7 @@ var param = {
   key: true
 };
 
-
-class game extends Phaser.Game {
+class Game extends Phaser.Game {
 
   constructor() {
     super(configPhaser);
@@ -15,7 +14,8 @@ class game extends Phaser.Game {
  
     //this.state.start('Boot');
     this.state.start('PlayGame', true, false, param);
+
   }
 }
 
-new game();
+var game = new Game();
