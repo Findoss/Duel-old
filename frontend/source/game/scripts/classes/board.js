e077a936a24f6a8e36bca4463299eb021a1ce176
Nikita Stroganov <findoss@yandex.ru>
2017-05-24 02:06:55 +0300

    добавлен класс поля, создан конструктор и методы загрузки поля и отображения

---
+++ frontend/source/game/scripts/classes/board.js
@@ -0,0 +1,69 @@
+class Board {
+
+  constructor(rows, columns, countGenerateRunes, minMoveCount) {
+    this.rows = rows || 6;
+    this.columns = columns || 6;
+    this.countGenerateRunes = countGenerateRunes || 6;
+    this.minMoveCount = minMoveCount || 3;
+
+    this.board = [];
+    this.moves = [];
+    this.clusters = [];
+  }
+
+  generation() {
+    /*do {
+      for (var i = 0; i < this.rows; i++) {
+        this.board[i] = [];
+        for (var j = 0; j < this.columns; j++) {
+          do {
+            newRune = randomIntegers(this.countRunes)+1;
+          }
+          while (this.isRuneInClusters(i, j, newRune) && !isClusters)
+          this.board[i][j] = { type: newRune, sprite: null }
+        }
+      }
+    }
+    while (this.findMoves() < this.minMoveCount);*/
+  }
+
+
+  load(savedBoard) {
+    this.board.length = 0;
+    this.rows = savedBoard.length;
+    this.columns = savedBoard[0].length;
+    for (var i = 0; i < this.rows; i++) {
+      this.board[i] = [];
+      for (var j = 0; j < this.columns; j++) {
+        this.board[i][j] = new Rune(savedBoard[i][j]);
+      }
+    }
+  }
+
+  viewCreate(marginBoardX, marginBoardY, marginRune) {
+    this.marginBoardX = marginBoardX || 150;
+    this.marginBoardY = marginBoardY || 150;
+    this.marginRune = marginRune || 10;
+    if (this.board) {
+      for (var i = 0; i < this.rows; i++) {
+        for (var j = 0; j < this.columns; j++) {
+          this.board[i][j].viewCreate(
+            this.marginBoardX + j * (textureRune.size.width+ this.marginRune),
+            this.marginBoardY + i * (textureRune.size.height + this.marginRune),
+            "sprite_"+this.board[i][j].type,
+            "pick",
+            false
+          );
+        }
+      }
+    }
+  }
+};
+
+/*
+for (var i = 0; i < this.rows; i++) {
+  for (var j = 0; j < this.columns; j++) {
+    new Rune();
+  }
+}
+*/
