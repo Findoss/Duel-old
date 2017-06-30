3b9c4e31d8f61e76bb6bd85d4b9e4225e8d24bd0
Nikita Stroganov <findoss@yandex.ru>
2017-07-01 00:59:14 +0300

    добавлено новое поле (старое состояние руны)

--- frontend/source/game/scripts/classes/rune.js
+++ frontend/source/game/scripts/classes/rune.js
@@ -1,13 +1,16 @@
 class Rune {
   constructor (type) {
     this.type = type
+    this.oldType = 0
   }
 
   newType (type) {
+    this.oldType = this.type
     this.type = type
   }
 
   newRandomType (count) {
+    this.oldType = this.type
     this.type = Math.floor(Math.random() * (count)) + 1
     return this.type
   }
