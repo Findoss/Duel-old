6d7fcbedb819787012036a20a364db4e5f48665e
Nikita Stroganov <findoss@yandex.ru>
2017-07-13 23:39:34 +0300

    код оптимизирован

--- frontend/source/game/scripts/states/PlayGame.js
+++ frontend/source/game/scripts/states/PlayGame.js
@@ -1,14 +1,11 @@
-/* global Phaser, param, game, textureRune, texturePath, testBoard5, Queue, Board, activeRune, View, debug, Debug, queue, utils, DEBUG_color, DEBUG_font */
+/* global Phaser, param, game, textureRune, texturePath, Queue, Board, activeRune, View, debug, Debug, queue, utils, DEBUG_color, DEBUG_font */
 
 let board = {}
 let view = {}
 let debug = {}
 let queue = {}
-let activeRune = null
 
-let board2 = {}
-let view2 = {}
-let finger = {}
+let activeRune = null
 
 let MESSAGE = ''
 let RUNES = []
@@ -38,34 +35,10 @@ class PlayGame extends Phaser.State {
     queue = new Queue()
     debug = new Debug(board, view, queue)
 
-    board2 = new Board()
-    view2 = new View(this, textureRune)
-
     this.bindEvents(board)
 
     // TEST ZONE
     board.loadBoard(param.board)
-
-    // board.generation(false)
-    /*
-    let XXX = 0
-    do {
-      let countMoves = board.findMoves()
-      if (countMoves) {
-        // console.log(board.moves)
-        for (var i = 0; i < countMoves; i++) {
-          view.hint(board.moves[i].b, board.moves[i].a, 'finger')
-        }
-        // let move = Math.floor(Math.random() * (countMoves))
-        // console.log(move)
-        // console.log(board.moves[move])
-        // board.swapRune(board.moves[move].b, board.moves[move].a)
-        // board.cleanMoves()
-      }
-      RUNES = board.loop()
-      XXX++
-    } while (XXX < 2)
-    */
   }
 
   update () {
@@ -73,12 +46,24 @@ class PlayGame extends Phaser.State {
   }
 
   render () {
-    game.debug.text('FPS: ' + this.game.time.fps, 20, 50, DEBUG_color, DEBUG_font)
-    game.debug.text('Q: ' + queue.queue.length, 150, 50, DEBUG_color, DEBUG_font)
-    game.debug.text(MESSAGE, 320, 50, '#ff0000', DEBUG_font)
-    if (activeRune !== null) this.game.debug.text('A: ' + view.getIndexs(activeRune).i + 'x' + view.getIndexs(activeRune).j, 230, 50, DEBUG_color, DEBUG_font)
+    game.debug.text('FPS: ' + this.game.time.fps, 20, 50, '#00ff00', '25px Arial')
+    game.debug.text('Q: ' + queue.queue.length, 150, 50, '#00ff00', '25px Arial')
+    game.debug.text(MESSAGE, 320, 50, '#ff0000', '25px Arial')
+    if (activeRune !== null) this.game.debug.text('A: ' + view.getIndexs(activeRune).i + 'x' + view.getIndexs(activeRune).j, 230, 50, '#00ff00', '25px Arial')
     for (let rune in RUNES) {
       game.debug.text(RUNES[rune], rune * 70, 830, '#ffffff', '30px Arial')
+    }
+  }
+
+  runeOver (rune) {
+    if (activeRune !== rune) {
+      rune.animations.play('focus', 1, true)
+    }
+  }
+
+  runeOut (rune) {
+    if (activeRune !== rune) {
+      rune.animations.play('wait', 4, true)
     }
   }
 
@@ -86,13 +71,13 @@ class PlayGame extends Phaser.State {
     if (activeRune === null) {
       activeRune = pickRune
       pickRune.animations.play('pick', 4, true)
+      queue.add(view, 'cleanHint', false)
     } else {
       let coordActiveRune = view.getIndexs(activeRune)
       if (!board.areTheSame(coordPickRune, coordActiveRune)) {
         if (board.isAdjacentRune(coordActiveRune, coordPickRune)) {
           if (!board.comparisonType(coordActiveRune, coordPickRune)) {
             board.swapRune(coordActiveRune, coordPickRune)
-            view.cleanHint()
             board.findClusters(coordActiveRune)
             board.findClusters(coordPickRune)
             if (board.clusters.length) {
@@ -106,11 +91,11 @@ class PlayGame extends Phaser.State {
                 board.deleteBoard()
                 board.generation(false)
               }
-              queue.add(view, 'renderHints', [board.findMoves(), 'finger'])
+              queue.add(view, 'renderHints', false, board.findMoves(), 'finger')
               board.cleanMoves()
             } else {
               board.swapRune(coordActiveRune, coordPickRune)
-              queue.add(view, 'renderHints', [board.findMoves(), 'finger'])
+              queue.add(view, 'renderHints', false, board.findMoves(), 'finger')
               board.cleanMoves()
             }
             activeRune.animations.play('wait', 4, true)
@@ -129,6 +114,8 @@ class PlayGame extends Phaser.State {
       } else {
         activeRune.animations.play('wait', 4, true)
         activeRune = null
+        queue.add(view, 'renderHints', false, board.findMoves(), 'finger')
+        board.cleanMoves()
       }
     }
   }
@@ -136,38 +123,32 @@ class PlayGame extends Phaser.State {
   bindEvents (board) {
     board.onLoad.add((newBoard) => {
       if (board) {
-        queue.add(view, 'renderBoard', [newBoard, 10, 100])
-        queue.add(view, 'renderHints', [board.findMoves(), 'finger'])
+        queue.add(view, 'renderBoard', true, newBoard, 10, 100)
+        queue.add(view, 'renderHints', false, board.findMoves(), 'finger')
         board.cleanMoves()
       }
     })
 
-    board.onLoad.add((newBoard) => {
-      if (board) {
-        view2.renderBoard(newBoard, 10, 100, 930)
-      }
-    })
-
     board.preSwap.add((coordRunes) => {
-      queue.add(view, 'renderSwap', [coordRunes[0], coordRunes[1]])
+      queue.add(view, 'renderSwap', true, coordRunes[0], coordRunes[1])
     })
 
     board.onRefill.add(function (coordRunes) {
       if (coordRunes.length) {
-        queue.add(view, 'renderRefill', [coordRunes, textureRune])
+        queue.add(view, 'renderRefill', true, coordRunes, textureRune)
       }
     })
 
     board.onDrop.add(function (dropRunes) {
       if (dropRunes.length) {
         for (let i = 0; i < dropRunes.length; i++) {
-          queue.add(view, 'renderSwap', [dropRunes[i][0], dropRunes[i][1], 45])
+          queue.add(view, 'renderSwap', true, dropRunes[i][0], dropRunes[i][1], 45)
         }
       }
     })
 
     board.onDeleteClusters.add(function (coordRunes) {
-      queue.add(view, 'renderDel', [coordRunes])
+      queue.add(view, 'renderDel', true, coordRunes)
     })
   }
 }
