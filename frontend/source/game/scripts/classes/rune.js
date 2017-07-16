db6c7b4fd5de1e30103234ffc563e6bd963db59a
Nikita Stroganov <findoss@yandex.ru>
2017-07-16 16:39:50 +0300

    добавлены коментарии

--- frontend/source/game/scripts/classes/rune.js
+++ frontend/source/game/scripts/classes/rune.js
@@ -1,17 +1,50 @@
+/**
+ * @class
+ * Типы рун
+ * 0 - пустая (удаленная) руна
+ * 1 - атакующая руна
+ * 2 - лечащая руна
+ * 3 - руна магии 1
+ * 4 - руна магии 2
+ * 5 - руна магии 3
+ * 6 - радужная руна
+ */
 class Rune {
+  /**
+   * Конструктор объекта руна
+   * @constructor
+   * @param  {!Number} type
+   */
   constructor (type) {
+    /**
+     * Тип руны
+     * @type {Number}
+     */
     this.type = type
+    /**
+     * Предыдущий тип руны
+     * @type {Number}
+     */
     this.oldType = 0
   }
 
+  /**
+   * Текущее значение сохраняет в `oldType` и устанавливает новый тип руны
+   * @param  {Number} type Тип руны
+   */
   newType (type) {
     this.oldType = this.type
     this.type = type
   }
 
-  newRandomType (count) {
+  /**
+   * Устанавливает случайный новый тип руны (от 1 до `maxCountType`)
+   * @param  {Number} maxCountType Максимальный допустимый тип руны
+   * @return {Number} Тип руны
+   */
+  newRandomType (maxCountType) {
     this.oldType = this.type
-    this.type = Math.floor(Math.random() * (count)) + 1
+    this.type = Math.floor(Math.random() * (maxCountType)) + 1
     return this.type
   }
 }
