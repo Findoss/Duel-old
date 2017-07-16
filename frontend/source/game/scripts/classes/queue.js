571fb6bed4f0d1704cfeb80b92aa2f6e2b2c42dd
Nikita Stroganov <findoss@yandex.ru>
2017-07-16 16:39:33 +0300

    добавлены коментарии

--- frontend/source/game/scripts/classes/queue.js
+++ frontend/source/game/scripts/classes/queue.js
@@ -1,17 +1,55 @@
 /* global Phaser */
 
+/**
+ * Очередь анимации
+ * @class
+ * @fires Queue
+ */
 class Queue {
+  /**
+   * @constructor
+   */
   constructor () {
+    /**
+     * Состояние очереди
+     * @type {Boolean}
+     * @default false
+     */
     this.isRender = false
+    /**
+     * Очередь
+     * @type {Array}
+     */
     this.queue = []
+    /**
+     * Вызывается после началом проигрывания очередной анимации/команды
+     * @event Queue#onPlay
+     */
     this.onPlay = new Phaser.Signal()
+    /**
+     * Вызывается после добаления анимации/команды в очередь
+     * @event Queue#onAdd
+     */
+    this.onAdd = new Phaser.Signal()
   }
 
+  /**
+   * Добавляет анимацию в очередь и вызывает функцию (`play()`)
+   * @param {Object}  context    контекст в котором будет исполняться анимация/команда
+   * @param {String}  command    название анимация/команда
+   * @param {Boolean} isBlocking TRUE - блокирущая очередь анимация/команда
+   *                             FALSE - НЕ блокирущая очередь анимация/команда
+   * @param {...*}    args       параметры команды
+   */
   add (context, command, isBlocking, ...args) {
     this.queue.push([{context, command, isBlocking, args}])
+    this.onAdd.dispatch(command)
     this.play()
   }
 
+  /**
+   * Воспроизведение анмации из очереди (рекурсия)
+   */
   play () {
     if (!this.isRender) {
       if (this.queue.length) {
