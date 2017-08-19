18792287f2c6afbf21fda0ea741625275c96c8e8
Nikita Stroganov <findoss@yandex.ru>
2017-08-20 01:36:34 +0300

    add sandbox

--- frontend/source/game/scripts/main.js
+++ frontend/source/game/scripts/main.js
@@ -11,10 +11,12 @@ class Game extends Phaser.Game {
     // this.state.add('Boot', Boot);
     // this.state.add('Preloader', Preloader);
     // this.state.add('MainMenu', MainMenu);
+    this.state.add('Sandbox', Sandbox, false)
     this.state.add('PlayGame', PlayGame, false)
 
     // this.state.start('Boot');
-    this.state.start('PlayGame', true, false, param)
+    // this.state.start('PlayGame', true, false, param)
+    this.state.start('Sandbox', true, false, param)
   }
 }
 
