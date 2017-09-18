7880f0172ae21ed620074584b5055ec1e48fd38a
Nikita Stroganov <findoss@yandex.ru>
2017-09-18 14:56:49 +0300

    добавление сборки документации в вики проекта

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,17 +1,27 @@
+const fs = require('fs')
+const path = require('path')
 const gulp = require('gulp')
 const livereload = require('gulp-livereload')
-const jsdoc = require('gulp-jsdoc3')
-const fs = require('fs')
 const jsdoc2md = require('jsdoc-to-markdown')
 
 gulp.task('doc', () => {
-  let config = require('./jsdoc.json')
-//   gulp.src(['./client/scripts/classes/*.js', '../README.md'], {read: false})
-//     .pipe(jsdoc(config))
-//     .pipe(livereload())
-// })
-  const output = jsdoc2md.renderSync({ files: './client/scripts/classes/*.js' })
-  fs.writeFileSync('../docs/api.md', output)
+  const inputFile = './client/scripts/classes/*.js'
+  const outputDir = '../docs/'
+  const templateData = jsdoc2md.getTemplateDataSync({ files: inputFile })
+
+  const classNames = templateData.reduce((classNames, identifier) => {
+    if (identifier.kind === 'class') classNames.push(identifier.name)
+    return classNames
+  }, [])
+
+  for (const className of classNames) {
+    console.log(`rendering ${className}`)
+    const output = jsdoc2md.renderSync({
+      data: templateData,
+      template: `{{#class name="${className}"}}{{>docs}}{{/class}} })`
+    })
+    fs.writeFileSync(path.resolve(outputDir, `class_${className}.md`), output)
+  }
 })
 
 gulp.task('doc_watch', () => {
