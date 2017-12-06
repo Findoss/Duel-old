83f7eeba8fb5c83e943726c44ed3b8c59ba15426
Nikita Stroganov <findoss@yandex.ru>
2017-12-06 14:38:06 +0300

    add template doc

--- utils/docs_build.js
+++ utils/docs_build.js
@@ -5,6 +5,7 @@ const jsdoc2md = require('jsdoc-to-markdown')
 // path
 const pathInput = 'client/scripts/classes/*.js'
 const pathOutput = 'docs/'
+const pathTemplate = 'utils/docs_template/members.hbs'
 
 let templateData = jsdoc2md.getTemplateDataSync({ files: pathInput })
 
@@ -19,7 +20,10 @@ for (let className of classNames) {
   console.log(`rendering class ${className}.js`)
   let output = jsdoc2md.renderSync({
     data: templateData,
-    template: `{{#class name="${className}"}}{{>docs}}{{/class}} })`
+    template: `{{#class name="${className}"}}{{>docs}}{{/class}} })`,
+    partial: [
+      pathTemplate
+    ]
   })
   fs.writeFileSync(path.resolve(pathOutput, `class_${className}.md`), output)
 }
