f80ece691b7e191b462bda7d38249c38ab75a45a
Nikita Stroganov <findoss@yandex.ru>
2017-12-13 17:27:54 +0300

    add gen board

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
+// vendor
 var app = require('express')
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
-var Board = require('./classes/board.js')
+// classes
+const Board = require('./classes/board')
+
+const board = new Board(5, 5, 3)
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
+  console.log('[server] user connected')
 
   socket.on('log', (msg) => {
-    console.log('log: ' + msg)
+    console.log('[ user ] log: ' + msg)
+  })
+
+  socket.on('game', (cmd) => {
+    console.log('[ user ] game: ' + cmd)
+    switch (cmd) {
+      case 'generation':
+        console.log('[server] emit generation board')
+        io.emit('generation', board.generation())
+        break
+      default:
+        console.log('err')
+    }
   })
 
   socket.on('disconnect', () => {
