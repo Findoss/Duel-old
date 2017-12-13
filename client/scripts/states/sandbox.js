5e59ee8a761aa7fb6d9971fe6ebc92eba25e00bb
Nikita Stroganov <findoss@yandex.ru>
2017-12-13 14:05:09 +0300

    add test texture

--- client/scripts/states/sandbox.js
+++ client/scripts/states/sandbox.js
@@ -1,11 +1,33 @@
+/* globals Phaser, textureSuggestion, configTextures */
+
 class Sandbox extends Phaser.State {
-  init () {}
+  init () {
+    // влючаем время для вывода FPS
+    this.game.time.advancedTiming = true
+    // влючаем возможность разворачивать на весь экран F11
+    this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL
+  }
 
-  preload () {}
+  preload () {
+    // загрузка руки (подсказка)
+    this.game.load.image(
+      textureSuggestion.fileName,
+      configTextures.path +
+      configTextures.skin +
+      textureSuggestion.fileName +
+      configTextures.ext
+    )
+  }
 
-  create () {}
+  create () {
+    // рендер руки
+    let suggestion = this.game.add.sprite(100, 100, textureSuggestion.fileName)
+  }
 
-  update () {}
+  update () {
+    // перехватываем ресайз игры и масштабируем
+    utils.resizeGame(this.gam
+  }
 
   render () {}
 }
