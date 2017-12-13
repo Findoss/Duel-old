d8f094efbfa420bb2dcc57877d6bb430476b7cc0
Nikita Stroganov <findoss@yandex.ru>
2017-12-13 18:05:48 +0300

    add more path

--- utils/docs_build.js
+++ utils/docs_build.js
@@ -3,27 +3,31 @@ const path = require('path')
 const jsdoc2md = require('jsdoc-to-markdown')
 
 // path
-const pathInput = 'client/scripts/classes/*.js'
+const pathInputs = [
+  'client/scripts/classes/*.js',
+  'server/classes/*.js'
+]
 const pathOutput = 'docs/'
 const pathTemplate = 'utils/docs_template/members.hbs'
 
-let templateData = jsdoc2md.getTemplateDataSync({ files: pathInput })
+for (var i = 0; i < pathInputs.length; i++) {
+  let templateData = jsdoc2md.getTemplateDataSync({ files: pathInputs[i] })
+  let classNames = templateData.reduce((classNames, identifier) => {
+    if (identifier.kind === 'class') {
+      classNames.push(identifier.name)
+    }
+    return classNames
+  }, [])
 
-let classNames = templateData.reduce((classNames, identifier) => {
-  if (identifier.kind === 'class') {
-    classNames.push(identifier.name)
+  for (let className of classNames) {
+    console.log(`rendering class ${className}.js`)
+    let output = jsdoc2md.renderSync({
+      data: templateData,
+      template: `{{#class name="${className}"}}{{>docs}}{{/class}} })`,
+      partial: [
+        pathTemplate
+      ]
+    })
+    fs.writeFileSync(path.resolve(pathOutput, `class_${className}.md`), output)
   }
-  return classNames
-}, [])
-
-for (let className of classNames) {
-  console.log(`rendering class ${className}.js`)
-  let output = jsdoc2md.renderSync({
-    data: templateData,
-    template: `{{#class name="${className}"}}{{>docs}}{{/class}} })`,
-    partial: [
-      pathTemplate
-    ]
-  })
-  fs.writeFileSync(path.resolve(pathOutput, `class_${className}.md`), output)
 }
