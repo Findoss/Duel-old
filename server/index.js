a9dd3f7ddc6ecd0091ac02c521cc20e604981db1
Nikita Stroganov <findoss@yandex.ru>
2017-12-17 19:23:34 +0300

    add debug

--- server/index.js
+++ server/index.js
@@ -5,33 +5,37 @@ var io = require('socket.io')(http)
 
 // classes
 const Board = require('./classes/board')
+const DEBUG = require('./configs/debug')
 
 const board = new Board()
 
+console.log('DEBUG.client: ' + DEBUG.client)
+console.log('DEBUG.server: ' + DEBUG.server)
+
 io.on('connection', (socket) => {
-  console.log('[server] user connected')
+  DEBUG.server && console.log('[.] user connected')
 
   socket.on('log', (msg) => {
-    console.log('[ user ] log: ' + msg)
+    DEBUG.client && console.log('[→] log: ' + msg)
   })
 
   socket.on('game', (cmd) => {
-    console.log('[ user ] game: ' + cmd)
+    DEBUG.client && console.log('[→] game: ' + cmd)
     switch (cmd) {
       case 'generation':
-        console.log('[server] emit generation board')
+        DEBUG.server && console.log('[.] generation board')
+        DEBUG.server && console.log('[←] new board')
         io.emit('generation', board.generation())
         break
       default:
-        console.log('err')
+        DEBUG.server && console.log('err')
     }
   })
 
   socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
+    DEBUG.server && console.log('[.] user disconnected')
 })
 
 http.listen(8080, () => {
-  console.log('listening on localhost:8080')
+  DEBUG.server && console.log('listening on localhost:8080')
 })
