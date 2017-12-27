64e646cf88cfa05a073cc485d8e0a4c2f9c1e53d
Nikita Stroganov <findoss@yandex.ru>
2017-12-28 01:29:06 +0300

    fix

--- client/scripts/states/sandbox.js
+++ client/scripts/states/sandbox.js
@@ -1,3 +1,4 @@
+<<<<<<< current
 /* globals Phaser, View, Queue, game, DEBUG, utils, socket, textureSuggestion, textureRune, configTextures */
 
 class Sandbox extends Phaser.State {
@@ -24,7 +25,7 @@ class Sandbox extends Phaser.State {
     )
 
     // загрузка рун
-    for (let i = 0; i < 6; i++) {
+    for (let i = 1; i < 7; i++) {
       this.game.load.spritesheet(
         textureRune.fileName + i,
         configTextures.path + textureRune.fileName + i + configTextures.ext,
@@ -82,3 +83,42 @@ class Sandbox extends Phaser.State {
     }
   }
 }
+=======
+/* globals Phaser, DEBUG, textureSuggestion, configTextures */
+
+class Sandbox extends Phaser.State {
+  init () {
+    // влючаем время для вывода FPS
+    this.game.time.advancedTiming = true
+    // влючаем возможность разворачивать на весь экран F11
+    this.game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL
+  }
+
+  preload () {
+    // загрузка руки (подсказка)
+    this.game.load.image(
+      textureSuggestion.fileName,
+      configTextures.path +
+      configTextures.skin +
+      textureSuggestion.fileName +
+      configTextures.ext
+    )
+  }
+
+  create () {
+    // рендер руки
+    let suggestion = this.game.add.sprite(100, 100, textureSuggestion.fileName)
+    suggestion.width = 50
+    suggestion.height = 50
+  }
+
+  update () {
+    // перехватываем ресайз игры и масштабируем
+    utils.resizeGame(this.game)
+  }
+
+  render () {
+    DEBUG.fps && game.debug.text('FPS: ' + this.game.time.fps, 20, 30, '#00ff00', '25px Arial')
+  }
+}
+>>>>>>> before discard
