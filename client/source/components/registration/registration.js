7383bea6fcaca8b18548999548c43460e28b07d2
Nikita Stroganov <findoss@yandex.ru>
2018-04-12 22:45:01 +0300

    valid form v0.2

--- client/source/components/registration/registration.js
+++ client/source/components/registration/registration.js
@@ -1,12 +1,14 @@
 export default {
   data() {
     return {
-      nicknameErrors: [],
-      emailErrors: [],
-      passwordErrors: [],
+      nicknameError: '',
+      emailError: '',
+      passwordError: '',
+
       nicknameStyles: '',
       emailStyles: '',
       passwordStyles: '',
+
       nickname: '',
       email: '',
       password: '',
@@ -14,53 +16,100 @@ export default {
   },
   methods: {
     submit() {
-      this.nicknameErrors = [];
-      this.emailErrors = [];
-      this.passwordErrors = [];
 
-      this.nicknameStyles = '';
-      this.emailStyles = '';
-      this.passwordStyles = '';
-
-
-      // this.nicknameErrors.push('Nickname is already taken');
-
-      // this.passwordErrors.push('Password is too short (minimum is 7 characters) and needs at least one number');
-      // this.passwordErrors.push('Password can\'t be blank');
-
-      // this.emailErrors.push('Email is invalid or already taken');
-      // this.emailErrors.push('Email can\'t be blank');
-
-
-      if (this.nickname === '') {
-        this.nicknameStyles = 'is-autocheck-error';
-        this.nicknameErrors.push('Nickname can\'t be blank');
-      } else {
-        console.log(this.nickname.length);
-        this.nicknameStyles = 'is-autocheck-successful';
+    },
+    validPassword(password) {
+      if (password !== '') {
+        if (password.length > 6) {
+          if (password.length < 20) {
+            if (/.(?=.*\d)(?=.*[a-zA-ZА-я])/.test(password)) {
+              return false;
+            }
+            return 'Password needs at least one number and at least one characters';
+          }
+          return 'Password is too long (maximum is 20 characters)';
+        }
+        return 'Password is too short (minimum is 7 characters)';
       }
-
-      if (this.email === '') {
-        this.emailStyles = 'is-autocheck-error';
-        this.emailErrors.push('Email can\'t be blank');
-      } else {
-        this.emailStyles = 'is-autocheck-successful';
-      }
-
-
-      if (this.password === '') {
-        this.passwordStyles = 'is-autocheck-error';
-        this.passwordErrors.push('Password can\'t be blank');
-      } else if (this.password.length < 7) {
-        this.passwordStyles = 'is-autocheck-error';
-        this.passwordErrors.push('Password is too short (minimum is 7 characters) and needs at least one number');
-      } else {
+      return 'Password can\'t be blank';
+    },
+    updatePassword(password) {
+      this.passwordError = '';
+      const result = this.validPassword(password);
+      if (!result) {
         this.passwordStyles = 'is-autocheck-successful';
+      } else {
+        this.passwordStyles = 'is-autocheck-error';
+        this.passwordError = result;
       }
     },
     validEmail(email) {
       const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+      if (email !== '') {
+        if (re.test(email)) {
+          return false;
+        }
+        return 'Email is invalid';
+      }
+      return 'Email can\'t be blank';
+    },
+    updateEmail(email) {
+      this.emailError = '';
+      const result = this.validEmail(email);
+      if (!result) {
+        fetch(`http://localhost:3001/check?email=${email}`)
+          .then(response =>
+            response.json())
+          .then((data) => {
+            if (data.used) {
+              this.emailStyles = 'is-autocheck-successful';
+            } else {
+              this.emailError = 'Nickname is already taken';
+              this.emailStyles = 'is-autocheck-error';
+            }
+          })
+          .catch(alert);
+      } else {
+        this.emailStyles = 'is-autocheck-error';
+        this.emailError = result;
+      }
+    },
+    validNickname(nickname) {
+      if (nickname !== '') {
+        if (nickname.length > 3) {
+          if (nickname.length < 20) {
+            if (/^[0-9a-zA-ZА-я_.-]+$/.test(nickname)) {
+              return false;
+            }
+            return 'Nickname contains invalid characters';
+          }
+          return 'Nickname is too long (maximum is 20 characters)';
+        }
+        return 'Nickname is too short (minimum is 4 characters)';
+      }
+      return 'Nickname can\'t be blank';
+    },
+    updateNickname(nickname) {
+      this.nicknameError = '';
+      const result = this.validNickname(nickname);
+      if (!result) {
+        this.nicknameStyles = 'is-autocheck-loading';
+        fetch(`http://localhost:3001/check?nickname=${nickname}`)
+          .then(response =>
+            response.json())
+          .then((data) => {
+            if (data.used) {
+              this.nicknameStyles = 'is-autocheck-successful';
+            } else {
+              this.nicknameError = 'Nickname is already taken';
+              this.nicknameStyles = 'is-autocheck-error';
+            }
+          })
+          .catch(alert);
+      } else {
+        this.nicknameStyles = 'is-autocheck-error';
+        this.nicknameError = result;
+      }
     },
   },
 };
