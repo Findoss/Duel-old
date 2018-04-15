1e27843f91b82eccbaf9f6d0a4fda0c963a515cc
Nikita Stroganov <findoss@yandex.ru>
2018-04-15 14:39:12 +0300

    update valid and add debounce

--- client/source/components/registration/registration.js
+++ client/source/components/registration/registration.js
@@ -1,4 +1,7 @@
 import UserService from '../../services/user-service';
+import Regexp from '../../modules/regexp';
+import debounce from 'debounce';
+
 
 export default {
 
@@ -12,95 +15,71 @@ export default {
       emailStyles: '',
       passwordStyles: '',
 
-      nickname: 'Ivan',
-      email: 'Ivan@email.ru',
-      password: 'password123',
+      nickname: '',
+      email: '',
+      password: '',
     };
   },
-  methods: {
-    submit() {
-      if (
-        // !this.validPassword(this.password) &&
-        // !this.validEmail(this.email) &&
-        // !this.validNickname(this.nickname)
-        true
-      ) {
-        const user = { nickname: this.nickname, email: this.email, password: this.password };
-        UserService.registration(user)
-          .then((result) => {
-            if (!result.error) console.log(result.message);
-            else console.error(result.error);
-          });
-      }
-    },
-    validPassword(password) {
-      if (password !== '') {
-        if (password.length > 6) {
-          if (password.length < 20) {
-            if (/.(?=.*\d)(?=.*[a-zA-ZА-я])/.test(password)) {
-              return false;
-            }
-            return 'Password needs at least one number and at least one characters';
-          }
-          return 'Password is too long (maximum is 20 characters)';
-        }
-        return 'Password is too short (minimum is 7 characters)';
-      }
-      return 'Password can\'t be blank';
-    },
-    updatePassword() {
-      this.passwordError = '';
-      const result = this.validPassword(this.password);
-      if (!result) {
-        this.passwordStyles = 'is-autocheck-successful';
-      } else {
-        this.passwordStyles = 'is-autocheck-error';
-        this.passwordError = result;
-      }
-    },
-    validEmail(email) {
-      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (email !== '') {
-        if (re.test(email)) {
-          UserService.checkEmail(email).then((result) => {
-            if (result.used) return false;
-            return 'Email is already taken';
-          });
-        } else return 'Email is invalid';
-      } else return 'Email can\'t be blank';
-    },
-    updateEmail() {
-      this.emailError = '';
-      this.emailStyles = 'is-autocheck-loading';
-      const result = this.validEmail(this.email);
-      setTimeout(() => {
-        if (!result) this.emailStyles = 'is-autocheck-successful';
-        else this.emailStyles = 'is-autocheck-error';
-        this.emailError = result;
-      }, 1000);
-    },
-    validNickname(nickname) {
-      if (nickname !== '') {
-        if (nickname.length > 3) {
-          if (nickname.length < 20) {
-            if (/^[0-9a-zA-ZА-я_.-]+$/.test(nickname)) {
-              UserService.checkNickname(nickname).then((result) => {
-                if (result.used) return false;
-                return 'Nickname is already taken';
-              });
-            } else return 'Nickname contains invalid characters';
-          } else return 'Nickname is too long (maximum is 20 characters)';
-        } else return 'Nickname is too short (minimum is 4 characters)';
-      } else return 'Nickname can\'t be blank';
-    },
-    updateNickname() {
+  created() {
+    this.updateNickname = debounce(() => {
+      const result = this.rulesNickname(this.nickname);
+      if (!result) this.nicknameStyles = 'is-autocheck-successful';
+      else this.nicknameStyles = 'is-autocheck-error';
+      this.nicknameError = result;
+    }, 500);
+    this.updateEmail = debounce(() => {
+      const result = this.rulesEmail(this.email);
+      if (!result) this.emailStyles = 'is-autocheck-successful';
+      else this.emailStyles = 'is-autocheck-error';
+      this.emailError = result;
+    }, 500);
+    this.updatePassword = debounce(() => {
+      const result = this.rulesPassword(this.password);
+      if (!result) this.passwordStyles = 'is-autocheck-successful';
+      else this.passwordStyles = 'is-autocheck-error';
+      this.passwordError = result;
+    }, 500);
+  },
+  watch: {
+    nickname() {
       this.nicknameStyles = 'is-autocheck-loading';
-      const result = this.validNickname(this.nickname);
-      setTimeout(() => {
-        if (!result) this.nicknameStyles = 'is-autocheck-successful';
-        else this.nicknameStyles = 'is-autocheck-error';
-        this.nicknameError = result;
-      }, 1000);
+      this.updateNickname();
+    },
+    email() {
+      this.emailStyles = 'is-autocheck-loading';
+      this.updateEmail();
+    },
+    password() {
+      this.passwordStyles = 'is-autocheck-loading';
+      this.updatePassword();
+    },
+  },
+  methods: {
+    rulesNickname(nickname) {
+      if (nickname === '') return 'Nickname can\'t be blank';
+      if (nickname.length < 4) return 'Nickname is too short (minimum is 4 characters)';
+      if (nickname.length > 20) return 'Nickname is too long (maximum is 20 characters)';
+      if (!Regexp.nickname.test(nickname)) return 'Nickname contains invalid characters';
+
+      UserService.checkNickname(nickname).then((result) => {
+        if (result.used) return false;
+        return 'Nickname is already taken';
+      });
+    },
+    rulesEmail(email) {
+      if (email === '') return 'Email can\'t be blank';
+      if (!Regexp.email.test(email)) return 'Email is invalid';
+      UserService.checkEmail(email).then((result) => {
+        if (!result.used) return false;
+        return 'Email is already taken';
+      });
+    },
+    rulesPassword(password) {
+      if (password === '') return 'Password can\'t be blank';
+      if (password.length < 6) return 'Password is too short (minimum is 7 characters)';
+      if (password.length > 20) return 'Password is too long (maximum is 20 characters)';
+      if (!Regexp.password.test(password)) return 'Password needs at least one number and at least one characters';
+      return false;
     },
   },
 };
