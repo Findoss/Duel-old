14b0326fc10a82f89a23a19c9590dd4b5e48332f
Nikita Stroganov <findoss@yandex.ru>
2018-04-19 23:36:27 +0300

    init base text field

---
+++ client/source/components/BaseTextField/BaseTextField.js
@@ -0,0 +1,91 @@
+export default {
+
+  name: 'text-field',
+
+  inheritAttrs: false,
+
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    initialValue: {
+      type: String,
+      default: '',
+    },
+    customClasses: {
+      type: String,
+    },
+    validationStatus: {
+      type: String,
+      default: '',
+      validator(value) {
+        return ['', 'valid', 'invalid', 'pending'].indexOf(value) !== -1;
+      },
+    },
+    validationError: {
+      type: String,
+      default: '',
+    },
+    validationIcon: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  data() {
+    return {
+      value: this.initialValue,
+    };
+  },
+
+  computed: {
+    classes() {
+      const classes = {
+        'base-input': true,
+      };
+
+      if (this.validationIcon) {
+        switch (this.validationStatus) {
+          case 'valid':
+            classes['is-autocheck-successful'] = true;
+            break;
+          case 'invalid':
+            classes['is-autocheck-error'] = true;
+            break;
+          case 'pending':
+            classes['is-autocheck-loading'] = true;
+            break;
+          default:
+            break;
+        }
+      }
+
+      if (this.customClasses) {
+        classes[this.customClasses] = true;
+      }
+
+      return classes;
+    },
+    // validationError() {
+
+    // }
+  },
+};
+
+/*
+  autocapitalize
+  autocorrect
+  placeholder
+  disabled
+  tabindex
+  autofocus
+*/
