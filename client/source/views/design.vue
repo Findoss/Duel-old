cc06a964081d1460e0c8538241c64be52fd45990
Nikita Stroganov <findoss@yandex.ru>
2018-04-19 23:37:42 +0300

    demo base text field

--- client/source/views/design.vue
+++ client/source/views/design.vue
@@ -12,70 +12,87 @@
   <p>
     1 2 3 4 5 6 7 8 9 0
   </p>
+  <br>
   <p>
-    Lorem <strong>ipsum dolor sit</strong>, amet consectetur adipisicing elit. <small>numquam eaque minima similique</small> laudantium voluptatem?
-  </p>
-  <p>
-    Текст на русском <strong>языке</strong>, <small>съешь еще</small> французских булок.
+    Lorem <strong>ipsum dolor sit</strong>, amet consectetur adipisicing <a href="">elit</a>. <small>numquam eaque minima similique</small> laudantium voluptatem?
   </p>
   <br>
+  <p>
+    Текст <a href="">на русском</a><strong> языке</strong>, <small>съешь еще</small> французских булок.
+  </p>
 
-  <a href="">Link</a>
-  <br><br>
+  <br>
+
+  <p class="error arrow-error ">arrow-error alert</p>
 
   <p class="error">error alert</p>
 
-  <table>
-    <thead>
-      <tr>
-        <th>Input</th>
-        <th>Example</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>text</td>
-        <td><input type="text" name="text" class="base-input"></input></td>
-      </tr>
-      <tr>
-        <td>disabled</td>
-        <td><input type="text" name="text" class="base-input" disabled></input></td>
-      </tr>
-      <tr>
-        <td>value</td>
-        <td><input type="text" name="text" class="base-input" value="Lorem ipsum dolor sit amet"></input></td>
-      </tr>
-      <tr>
-        <td>loading</td>
-        <td><input type="text" name="text" class="base-input  is-autocheck-loading"></input></td>
-      </tr>
-      <tr>
-        <td>successful</td>
-        <td><input type="text" name="text" class="base-input is-autocheck-successful"></input></td>
-      </tr>
-      <tr>
-        <td>error</td>
-        <td>
-          <input type="text" name="text" class="base-input is-autocheck-error"></input>
-          <p v-if="error" class="error arrow-error">{{ error }}</p>
-        </td>
-      </tr>
-      <tr>
-        <td>placeholder</td>
-        <td>
-            <input type="text" name="password" class="base-input" placeholder="placeholder"></input>
-        </td>
-      </tr>
-      <tr>
-        <td>password</td>
-        <td><input type="password" class="base-input" name="password"></input></td>
-      </tr>
-      <tr>
-        <td>button</td>
-        <td><button type="submit" class="base-button">Hello Button</button></td>
-      </tr>
-    </tbody>
-  </table>
+  <br>
+
+  <text-field
+    label="text-field rules"
+    name="field1"
+    v-model="field.value"
+    :validationStatus="field.status"
+    :validationError="field.error"
+    @input="validation(field)"
+  />
+
+  <text-field
+    label="text-field rules - icon off"
+    name="field9"
+    :validationIcon="false"
+    v-model="field2.value"
+    :validationStatus="field2.status"
+    :validationError="field2.error"
+    @input="validation(field2)"
+  />
+
+  <text-field
+    label="text-field disabled"
+    name="field2"
+    disabled="disabled"
+  />
+
+  <text-field
+    label="text-field value"
+    name="field3"
+    initialValue="Lorem ipsum dolor sit amet"
+  />
+
+  <text-field
+    label="text-field placeholder"
+    name="field4"
+    placeholder="placeholder"
+  />
+
+  <text-field
+    label="text-field password"
+    name="field5"
+    type="password"
+  />
+
+  <text-field
+    label="text-field - pending"
+    name="field6"
+    validationStatus="pending"
+  />
+
+  <text-field
+    label="text-field - valid"
+    name="field7"
+    validationStatus="valid"
+  />
+
+  <text-field
+    label="text-field - invalid + error"
+    name="field8"
+    validationStatus="invalid"
+    validationError="error"
+  />
+
+  <button type="submit" class="base-button">Hello Button</button>
+
   <br>
 
 </div>
@@ -83,7 +100,6 @@
 
 <style scoped>
   @import "../styles/typography.css";
-  @import "../styles/input.css";
   @import "../styles/link.css";
   @import "../styles/button.css";
   @import "../styles/card.css";
@@ -92,11 +108,50 @@
 </style>
 
 <script>
+import UserService from '@/services/user-service';
+import Rules from '@/modules/validation-rules';
+import BaseTextField from '@/components/BaseTextField/BaseTextField.vue'
+
 export default {
+
+  components: {
+    'text-field': BaseTextField
+  },
+
   data() {
     return {
       error: 'Ошибка введенных данных, Error entering data',
+      field: {
+        value: '',
+        error: '',
+        status: '',
+        rules: (value) => {
+          return value.length > 3
+        }
+      },
+      field2: {
+        value: '',
+        error: '',
+        status: '',
+        rules: (value) => {
+          return value.length > 3
+        }
+      }
     }
-  }
+  },
+
+  methods: {
+    validation(formField) {
+      const field = formField;
+      field.error = '';
+      field.status = 'pending';
+      if (field.rules(field.value)) {
+        field.status = 'valid';
+      } else {
+        field.status = 'invalid';
+        field.error = 'error';
+      }
+    }
+  },
 };
 </script>
