bb2a505191447eb0a8168e1ce609cf9f31d2cfdf
Nikita Stroganov <findoss@yandex.ru>
2018-04-21 11:29:36 +0300

    signin new input

--- client/source/views/signin/signin.js
+++ client/source/views/signin/signin.js
@@ -1,4 +1,3 @@
-import debounce from 'debounce';
 import Rules from '@/modules/validation-rules';
 import UserService from '@/services/user-service';
 import BaseTextField from '@/components/BaseTextField/BaseTextField.vue';
@@ -14,44 +13,23 @@ export default {
       form: {
         error: '',
         email: {
-          error: '',
           value: '',
-          status: '',
-          rules: Rules.email,
+          status: false,
+          rules: [
+            Rules.email,
+          ],
         },
         password: {
           value: '',
-          error: '',
-          status: '',
         },
       },
     };
   },
 
-  created() {
-    this.validation = debounce((formField) => {
-      const field = formField;
-      field.error = '';
-      field.status = 'pending';
-      field.rules(field.value)
-        .then(() => {
-          field.status = 'valid';
-        })
-        .catch((error) => {
-          field.status = 'invalid';
-          field.error = error.message;
-        });
-    }, 500);
-  },
-
   methods: {
     submit() {
-      if (this.form.email.status !== 'valid') {
-        return this.validation(this.form.email);
-      } else if (this.form.password.value === '') {
-        this.form.password.status = 'invalid';
-        this.form.password.error = 'Password can\'t be blank';
-        return false;
+      if (!this.form.email.status) {
+        return this.$refs.email.validation();
       }
 
       const user = {
