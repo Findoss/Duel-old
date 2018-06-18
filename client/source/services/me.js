0f844462a93d52720607eb8175ef483dc200318f
Nikita Stroganov <findoss@yandex.ru>
2018-06-18 20:22:09 +0300

    update API me

--- client/source/services/me.js
+++ client/source/services/me.js
@@ -6,4 +6,4 @@ export const deleteAccount = () => Http.delete('/me');
 
 export const updateAccount = userData => Http.patch('/me', userData);
 
-export const updateAccountPass = passwords => Http.post('/me/password', passwords);
+export const updateAccountPass = passwords => Http.patch('/me/password', passwords);
