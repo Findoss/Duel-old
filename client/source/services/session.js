1fa5608e595b2ceba3350ad857f22e3f78a78926
Nikita Stroganov <findoss@yandex.ru>
2018-06-18 16:02:04 +0300

    optimaze session

--- client/source/services/session.js
+++ client/source/services/session.js
@@ -1,21 +1,24 @@
 import Http from '@/utils/http';
 
-export const signIn = user => Http.post('/signin', user).then((response) => {
-  if (response.code === undefined) {
-    localStorage.setItem('session-token', response.token);
-  }
-  return response;
+export const signIn = user => Http.post('/signin', user)
+  .then((response) => {
+    if (response.code === undefined) {
+      localStorage.setItem('session-token', response.token);
+    }
+    return response;
+  });
+
+export const signOut = () => new Promise((resolve, reject) => {
+  Http.delete('/signout')
+    .then((response) => {
+      if (response.code === undefined) {
+        localStorage.removeItem('session-token');
+        resolve(response);
+      }
+      reject(response);
+    });
 });
 
-export const signOut = () =>
-// return Http.delete('/signout').then((result) => {
-//   if (result.code === undefined) {
-//     localStorage.removeItem('session-token');
-//     return true;
-//   }
-//   return false;
-// });
-  localStorage.removeItem('session-token');
 export const getToken = () => localStorage.getItem('session-token');
 
-export const signedIn = () => !!localStorage.getItem('session-token');
+export const isLogin = () => Boolean(localStorage.getItem('session-token'));
