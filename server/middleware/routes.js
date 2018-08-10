22e3fd4ebfb77fca1fc0ebc8e16f59d268a6e6c7
Nikita Stroganov <findoss@yandex.ru>
2018-08-10 12:44:08 +0300

    add user route

--- server/middleware/routes.js
+++ server/middleware/routes.js
@@ -1,38 +1,20 @@
 const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
 
-const router = new Router();
+// controllers
+const ctrlUser = require('../controllers/user');
 
 function getAll() {
   return new Promise((resolve, reject) => {
     resolve({ hello: 'world' });
   });
 }
+const router = new Router();
 
 
 router
-  .get('/product', async (ctx, next) => {
-    ctx.body = await getAll();
+  .get('/users', ctrlUser.getUsers)
+  .get('/users/:id', ctrlUser.getUser)
+  .post('/users', ctrlUser.userCreate)
   });
-// .get('/product/:id', async (ctx, next) => {
-//   let result = await product.get(ctx.params.id);
-//   if (result) {
-//     ctx.body = result
-//   } else {
-//     ctx.status = 204
-//   }
-// })
-// .post('/product', koaBody, async (ctx, next) => {
-//   ctx.status = 201;
-//   ctx.body = await product.create(ctx.request.body)
-// })
-// .put('/product/:id', koaBody, async (ctx, next) => {
-//   ctx.status = 204;
-//   await product.update(ctx.params.id, ctx.request.body);
-// })
-// .delete('/product/:id', async (ctx, next) => {
-//   ctx.status = 204;
-//   await product.delete(ctx.params.id);
-// });
 
 module.exports = () => router.routes();
