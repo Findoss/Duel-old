0cc94308e9d8cd3bacc05165c3f144ef3e515197
Nikita Stroganov <findoss@yandex.ru>
2018-08-10 12:44:49 +0300

    add static route

--- server/middleware/routes.js
+++ server/middleware/routes.js
@@ -2,12 +2,17 @@ const Router = require('koa-router');
 
 // controllers
 const ctrlUser = require('../controllers/user');
+const ctrlTool = require('../controllers/tool');
 
 function getAll() {
   return new Promise((resolve, reject) => {
     resolve({ hello: 'world' });
   });
 }
+// static
+const avatars = require('../static/avatars.json');
+const userParameters = require('../static/user_parameters.json');
+
 const router = new Router();
 
 
@@ -15,6 +20,19 @@ router
   .get('/users', ctrlUser.getUsers)
   .get('/users/:id', ctrlUser.getUser)
   .post('/users', ctrlUser.userCreate)
+  .head('/ping', async (ctx, next) => {
+    ctx.status = 200;
+    next();
+  })
+
+  .get('/static/avatars', async (ctx, next) => {
+    ctx.body = avatars;
+    next();
+  })
+
+  .get('/static/user-parameters', async (ctx, next) => {
+    ctx.body = userParameters;
+    next();
   });
 
 module.exports = () => router.routes();
