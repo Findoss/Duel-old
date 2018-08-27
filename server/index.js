03aa43ed36f630000967e008bbd45b9ea2ee418f
Nikita Stroganov <findoss@yandex.ru>
2018-08-27 20:33:02 +0300

    установлен порядок запуска
    сначала подключаемся к бд только потом запускаем сервер

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ const config = require('./config/index');
 
 // Koa
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
 
 // middleware
 const time = require('./middleware/time');
@@ -13,25 +13,35 @@ const headers = require('./middleware/headers');
 const passport = require('koa-passport');
 
 // db
+const mongoose = require('mongoose');
 require('./models/db');
 require('./controllers/passport');
 
-function createApp() {
+async function createApp() {
   const app = new Koa();
 
   app.use(time);
+  if (config.logger.koa) app.use(logger());
   app.use(error);
-  app.use(logger());
   app.use(bodyParser());
   app.use(passport.initialize());
   app.use(routes());
   app.use(headers);
 
-  return app;
+  await mongoose.connect(
+    `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`,
+    { useNewUrlParser: true },
+  );
+
+  const server = await app.listen(config.node.port);
+  if (config.logger.node) console.log(`[server  ] start - ${config.node.host}:${config.node.port}`);
+
+  return server;
 }
 
 if (!module.parent) {
-  createApp().listen(config.node.port);
+  // createApp().listen(config.node.port);
+  createApp();
 }
 
 module.exports = createApp;
