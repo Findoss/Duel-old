950cddb9bafe0680faf4cd2f70b0348ce54362c3
Nikita Stroganov <findoss@yandex.ru>
2018-09-09 21:00:02 +0300

    добавление Skill API
    добавление тестов
    добавление роутов
    добавление контроллеров
    добавление модели для базы

--- server/middleware/routes/index.js
+++ server/middleware/routes/index.js
@@ -5,12 +5,14 @@ const routeStatics = require('./statics');
 const routeUsers = require('./users');
 const routeTools = require('./tools');
 const routeAuth = require('./auth');
+const routeSkill = require('./skills');
 
 
 const router = new Router();
 
 router
   .use('/static', routeStatics.routes())
+  .use('/skills', routeSkill.routes())
   .use('/users', routeUsers.routes())
   .use('/tools', routeTools.routes())
   .use('/auth', routeAuth.routes());
