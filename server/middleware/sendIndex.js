0e9749910661f9cb4877449a718a17db09479ead
Nikita Stroganov <findoss@yandex.ru>
2018-09-29 21:01:20 +0300

    fix не отдавать статику на запросы к API

--- server/middleware/sendIndex.js
+++ server/middleware/sendIndex.js
@@ -1,7 +1,9 @@
 const send = require('koa-send');
 
-module.exports = async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8;';
-  await send(ctx, '/index.html', { root: '../client/build/' });
+module.exports = () => async (ctx, next) => {
+  if (ctx.response.status !== 200 && ctx.response.status !== 201) {
+    ctx.type = 'text/html; charset=utf-8;';
+    await send(ctx, '/index.html', { root: '../client/build/' });
+  }
   await next();
 };
