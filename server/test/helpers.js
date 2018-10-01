6be1b5c904442d1d4a60ebbacd51d137186d1c73
Nikita Stroganov <findoss@yandex.ru>
2018-10-02 01:31:40 +0300

    обновил утилитарные функции для тестов при работе с бд

--- server/test/helpers.js
+++ server/test/helpers.js
@@ -8,43 +8,28 @@ const Skill = require('./../models/skill');
 
 // const
 const mongoImport = `mongoimport -d ${config.db.name}`;
-
-// controllers
-const { generateToken } = require('./../controllers/token');
-
 const cmd = command => new Promise((res, rej) => exec(command, err => (err ? rej(err) : res())));
 
 
 module.exports.loadCollection = async (collection, file) => cmd(`${mongoImport} -c ${collection} --file ${file}`);
 
 module.exports.clearUsers = async () => User.remove({});
-module.exports.clearSkill = async () => Skill.remove({});
+module.exports.clearSkills = async () => Skill.remove({});
 module.exports.clearSessions = async () => Session.remove({});
 
 
 // todo
 
-module.exports.buySkills = async (userId, skills) => {
-  const users = await User.find({});
-
+module.exports.buySkills = async (userId, ...skillsId) => {
   await User.findByIdAndUpdate(
-    users[userId].id,
-    { $push: { skillsUnlocked: skills.map(skill => skill.id) } },
+    userId,
+    { $push: { skillsUnlocked: skillsId } },
   );
 };
 
-module.exports.addSkillSet = async (userId, skills) => {
-  const users = await User.find({});
-
+module.exports.addSkillSet = async (userId, ...skillsId) => {
   await User.findByIdAndUpdate(
-    users[userId].id,
-    { $push: { skillSet: skills.map(skill => skill.id) } },
+    userId,
+    { $push: { skillSet: skillsId } },
   );
-};
-
-module.exports.signigUser = async (id) => {
-  const users = await User.find({});
-  const userId = users[id].id;
-  const token = await generateToken(userId);
-  return `Bearer ${token}`;
 };
