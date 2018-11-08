c961e6db677f85bf64336c24f70bb0484a3de35a
Nikita Stroganov <findoss@yandex.ru>
2018-11-08 22:56:18 +0300

    удалил ненужный роутинг

--- client/source/App.vue
+++ client/source/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
     <div>statusSocket {{ this.$store.state.statusSocket }}</div>
-    <div id="nav">
-      fast navigate
-      <router-link to="/signin">singin</router-link> |
-      <router-link to="/1">profile-overview</router-link>  |
-      <router-link to="/design">design</router-link>  |
-    </div>
     <router-view/>
   </div>
 </template>
