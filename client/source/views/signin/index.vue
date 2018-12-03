51cf61ca37cb4536343c15d550657dedf9b82694
Nikita Stroganov <findoss@yandex.ru>
2018-12-04 02:25:52 +0300

    update view signin

--- client/source/views/signin/index.vue
+++ client/source/views/signin/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>{{ $t('hello') }}</div>
+  <div class="container">
+    <div class="paralax"></div>
+    <div class="sidebar">
+      <z-logo class="sidebar_logo" />
+      <z-title
+        class="sidebar_title"
+        :size="1"
+      >
+        {{ $t('signInDuel') }}
+      </z-title>
+      <div class="sidebar_form-signin">
+        <z-button>{{ $t('signin')}}</z-button>
+      </div>
+      <span class="sidebar_new-account">
+        {{ $t('registration')}}
+        <router-link to="/registration">
+          {{ $t('createAccount') }}
+        </router-link>
+      </span>
+    </div>
+  </div>
 </template>
 <script src="./script.js"></script>
 <style src="./style.css" scoped></style>
