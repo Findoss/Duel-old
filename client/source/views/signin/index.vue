8a3faa97d5195b24608ace29b19ea8ccce4e2b81
Nikita Stroganov <findoss@yandex.ru>
2018-12-04 21:59:50 +0300

    update

--- client/source/views/signin/index.vue
+++ client/source/views/signin/index.vue
@@ -9,9 +9,77 @@
       >
         {{ $t('signInDuel') }}
       </z-title>
-      <div class="sidebar_form-signin">
-        <z-button>{{ $t('signin')}}</z-button>
-      </div>
+
+      <form
+        accept-charset="UTF-8"
+        @submit.prevent="submit"
+        @keyup.enter="submit"
+        novalidate="true"
+        class="sidebar_form-signin"
+      >
+        <!-- <z-alert
+            :icon="true"
+            :dismissible="true"
+            :type="alert.type"
+            :show="alert.message"
+            @close="closeAlert()"
+          >
+            {{ alert.message }}
+          </z-alert> -->
+
+        <z-input
+          :validationIcon="false"
+          :validationRules="form.email.rules"
+          @validation="form.email.status = $event"
+          autofocus="autofocus"
+          name="email"
+          placeholder="email@email.com"
+          ref="email"
+          tabindex="1"
+          type="email"
+          v-model="form.email.value"
+        />
+
+        <div>
+          <z-input
+            :validationIcon="false"
+            :validationRules="form.password.rules"
+            @validation="form.password.status = $event"
+            name="password"
+            placeholder="your password"
+            ref="password"
+            tabindex="2"
+            type="password"
+            v-model="form.password.value"
+          />
+          <div class="form-signin_forgot-password">
+            <router-link to="/password-reset">{{ $t('forgotPassword') }}</router-link>
+          </div>
+        </div>
+
+        <z-button
+          tabindex="3"
+          type="submit"
+        >{{ $t('signin')}}</z-button>
+
+        <span class="form-signin_or">{{ $t('or') }}</span>
+
+        <div class="form-signin_socials-networks">
+          <img
+            src="../../assets/icons/social_networks/fb.png"
+            alt="fb"
+          >
+          <img
+            src="../../assets/icons/social_networks/vk.png"
+            alt="vk"
+          >
+          <img
+            src="../../assets/icons/social_networks/gh.png"
+            alt="gh"
+          >
+        </div>
+      </form>
+
       <span class="sidebar_new-account">
         {{ $t('registration')}}
         <router-link to="/registration">
--- client/source/views/signin/script.js
+++ client/source/views/signin/script.js
@@ -1,1 +1,68 @@
-export default {};
+import { mapActions } from 'vuex';
+
+// Utils
+import Rules from '@/utils/validation/rules';
+import validationForm from '@/utils/validation/form';
+
+export default {
+
+  data() {
+    return {
+      form: {
+        email: {
+          value: '',
+          status: false,
+          rules: [Rules.email],
+        },
+        password: {
+          value: '',
+          status: false,
+          rules: [Rules.required],
+        },
+      },
+    };
+  },
+
+  computed: {
+    // alert() {
+    //   return this.$store.state.me.account.alertSignin;
+    // },
+  },
+
+  methods: {
+    ...mapActions({
+      signIn: 'me/account/signIn',
+      // showAlert: 'me/account/showAlertSignin',
+    }),
+
+    submit() {
+      if (!validationForm(this, 'form')) return false;
+
+      const user = {
+        email: this.form.email.value,
+        password: this.form.password.value,
+      };
+
+      this.signIn(user)
+        .catch(() => {
+          this.$refs.password.reset();
+          this.form.password.status = false;
+        });
+    },
+
+    // closeAlert() {
+    //   this.showAlert({
+    //     type: 'error',
+    //     message: null,
+    //   });
+    // },
+  },
+
+  // beforeRouteLeave(to, from, next) {
+  //   this.showAlert({
+  //     type: 'info',
+  //     message: '',
+  //   });
+  //   next();
+  // },
+};
