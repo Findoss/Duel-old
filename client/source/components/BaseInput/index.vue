af1d64cd618a286df32d7d62eb7a18707f76e373
Nikita Stroganov <findoss@yandex.ru>
2018-12-04 21:58:44 +0300

    add Input

---
+++ client/source/components/BaseInput/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="base-input-group">
+    <label
+      v-if="label"
+      for="name"
+    >{{ label }}</label>
+    <input
+      :id="name"
+      :name="name"
+      :type="type"
+      :class="classes"
+      :value="value"
+      @input="updateField($event.target.value)"
+      v-bind="$attrs"
+    >
+    <p
+      v-if="error"
+      class="base-input-group__error"
+    >{{ $t(`form.errors[${error}]`) }}</p>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
