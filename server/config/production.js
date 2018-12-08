769bd22ceab6390da4fce47bb82bbc2fe467e670
Nikita Stroganov <findoss@yandex.ru>
2018-12-08 14:00:32 +0300

    fix global config type

--- server/config/production.js
+++ server/config/production.js
@@ -1,8 +1,6 @@
-console.log(process.env.LOGGER_KOA);
-console.log(typeof process.env.LOGGER_KOA);
-console.log(!!process.env.LOGGER_KOA);
-console.log(process.env.LOGGER_KOA !== 'false');
-
+function toBoolean(variable) {
+  return variable !== 'false';
+}
 
 module.exports = {
   db: {
@@ -19,16 +17,16 @@ module.exports = {
   email: {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE,
+    secure: toBoolean(process.env.SMTP_SECURE),
     address: process.env.SMTP_ADDRESS,
     username: process.env.SMTP_USERNAME,
     password: process.env.SMTP_PASSWORD,
   },
   logger: {
-    db: process.env.LOGGER_DB,
-    node: process.env.LOGGER_NODE,
-    koa: process.env.LOGGER_KOA,
-    i18n: process.env.LOGGER_I18N,
+    db: toBoolean(process.env.LOGGER_DB),
+    node: toBoolean(process.env.LOGGER_NODE),
+    koa: toBoolean(process.env.LOGGER_KOA),
+    i18n: toBoolean(process.env.LOGGER_I18N),
   },
   JWTKey: process.env.JWT_KEY,
 };
