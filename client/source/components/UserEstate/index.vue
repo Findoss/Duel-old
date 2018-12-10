a610e803e3a7199ec796a72ff921a7fb94713d1f
Nikita Stroganov <findoss@yandex.ru>
2018-12-11 01:13:06 +0300

    add user estate

---
+++ client/source/components/UserEstate/index.vue
@@ -0,0 +1,9 @@
+
+<template>
+  <div>
+    <span class="gold"><strong>{{ estate.gold }} </strong></span>
+    <span class="point"><strong>{{ estate.points }}</strong></span>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
---
+++ client/source/components/UserEstate/script.js
@@ -0,0 +1,14 @@
+export default {
+
+  name: 'z-user-estate',
+
+  props: {
+    estate: {
+      type: Object,
+      default: {
+        gold: 0,
+        points: 0,
+      },
+    },
+  },
+};
