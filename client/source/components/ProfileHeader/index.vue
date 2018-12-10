dd1d88a8e3e0e999251627b71056156a01392a11
Nikita Stroganov <findoss@yandex.ru>
2018-12-11 01:11:29 +0300

    add profile header

---
+++ client/source/components/ProfileHeader/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="profile-header">
+    <!-- <z-user-estate /> -->
+    <div>
+      XXX__XXXXXXXXXXXXXXXXXXXXX <br>
+      X__XXXX___X__XXX_BUY
+    </div>
+    <z-profile-navigation class="profile-header__profile-navigation" />
+    <z-setting-navigation class="profile-header__setting-navigation" />
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
