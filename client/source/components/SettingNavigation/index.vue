a87dc4d740407a6bab1d41069fc0ce55fb5a1138
Nikita Stroganov <findoss@yandex.ru>
2018-12-11 01:12:15 +0300

    add setting navigation

---
+++ client/source/components/SettingNavigation/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="setting-navigation">
+    <div class="setting-navigation__item">⊠</div>
+    <div class="setting-navigation__item">⊠</div>
+    <div class="setting-navigation__item">⊠</div>
+    <div class="setting-navigation__item">⊠</div>
+    <div class="setting-navigation__item">⊠</div>
+    <div class="setting-navigation__item">⊠</div>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
