bef3ae9757ff10e43de62857909d53b9c1560e3e
Nikita Stroganov <findoss@yandex.ru>
2018-12-12 15:03:06 +0300

    add container profile

--- client/source/components/UserEstate/index.vue
+++
@@ -1,9 +0,0 @@
-
-<template>
-  <div>
-    <span class="gold"><strong>{{ estate.gold }} </strong></span>
-    <span class="point"><strong>{{ estate.points }}</strong></span>
-  </div>
-</template>
-<script src="./script.js"></script>
-<style src="./style.css" scoped></style>
---
+++ client/source/containers/profile/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="profile-container">
+    <z-profile-header class="profile-container__header" />
+    <div class="profile-container__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
