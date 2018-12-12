cb8debffa4e4802e2f6c46b902b01435bc9961bc
Nikita Stroganov <findoss@yandex.ru>
2018-12-12 15:02:45 +0300

    add container guest

---
+++ client/source/containers/guest/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="paralax"></div>
+    <div class="sidebar">
+      <z-logo class="sidebar__logo" />
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
