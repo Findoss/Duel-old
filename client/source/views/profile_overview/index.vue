0703a49ce75dcbdb8b5475adeb4348ef5c0a6380
Nikita Stroganov <findoss@yandex.ru>
2018-12-12 15:10:33 +0300

    update profile,  use container

--- client/source/views/profile_overview/index.vue
+++ client/source/views/profile_overview/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <div class="profile-container">
-    <z-profile-header class="profile-container__header" />
-    <div class="profile-container__content">
-      <z-button @click="signOut()">sign out</z-button>
-      <z-button @click="privateSend()">private click</z-button>
-      <pre v-html="userData"></pre>
-    </div>
-  </div>
-
+  <z-container-profile>
+    <z-button @click="signOut()">sign out</z-button>
+    <z-button @click="privateSend()">private click</z-button>
+    <pre v-html="userData"></pre>
+  </z-container-profile>
 </template>
 <script src="./script.js"></script>
 <style src="./style.css" scoped></style>
