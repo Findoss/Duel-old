7ce8601103d8d59b4207e31ad11e9829bd3b1f70
Nikita Stroganov <findoss@yandex.ru>
2018-12-17 21:39:48 +0300

    update header

--- client/source/components/ProfileMenu/index.vue
+++ client/source/components/ProfileMenu/index.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="profile-menu">
     <router-link
+      class="profile-menu__item button-play button-icon__skill"
+      :to="{ name: 'profile'}"
+    >
+      {{ $t('menu.play') }}
+    </router-link>
+    <router-link
       class="profile-menu__item button-icon__skill"
       :to="{ name: 'profile'}"
     >
       {{ $t('menu.profile') }}
     </router-link>
-    <router-link
+    <!-- <router-link
       class="profile-menu__item button-icon__skill"
       :to="{ name: 'skills'}"
     >
       {{ $t('menu.skills') }}
-    </router-link>
-    <router-link
+    </router-link> -->
+    <!-- <router-link
       class="profile-menu__item button-icon__skill"
       :to="{ name: 'inventory'}"
     >
       {{ $t('menu.inventory') }}
-    </router-link>
+    </router-link> -->
     <!-- <router-link
-      class="profile-menu__item button-play button-icon__skill"
-      :to="{ name: 'root'}"
-    >
-      {{ $t('menu.play') }}
-    </router-link>
-    <router-link
       class="profile-menu__item button-icon__skill"
       :to="{ name: 'shop'}"
     >
