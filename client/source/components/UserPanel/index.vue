36dc433ccc05423c3474863bf7d7b1b7022464a6
Nikita Stroganov <findoss@yandex.ru>
2018-12-17 21:40:12 +0300

    add user Panel

---
+++ client/source/components/UserPanel/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="user-panel">
+    <z-title class="user-panel__skills-title">user-skills</z-title>
+    <z-user-skillset class="user-panel__skillset" />
+    <z-title class="user-panel__items-title">user-items</z-title>
+    <z-user-items class="user-panel__items" />
+  </div>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
