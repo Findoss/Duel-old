c5d03ce42fc15dee5f0f50698f45f4d8daa3e468
Nikita Stroganov <findoss@yandex.ru>
2018-12-20 15:16:09 +0300

    add other user

--- client/source/views/profile_overview/script.js
+++ client/source/views/profile_overview/script.js
@@ -7,27 +7,51 @@ export default {
     'z-container-profile': ContainerProfile,
   },
 
+  data() {
+    return {
+      pending: false,
+    };
+  },
+
   computed: {
     ...mapGetters([
       'myId',
     ]),
     ...mapGetters({
-      userData: 'me/getAllData',
+      me: 'me/getAllData',
+      opponent: 'opponent/getAllData',
     }),
 
     isMe() {
       return this.myId === this.$route.params.userId;
+    },
+
+    user() {
+      if (this.isMe) return this.me;
+      return this.opponent;
     },
   },
 
   methods: {
     ...mapActions({
       getMe: 'me/loadMe',
-      getUser: 'me/loadUser',
+      getOtherUser: 'opponent/loadUser',
     }),
+
+    getUser() {
+      return this.getOtherUser(this.$route.params.userId);
+    },
   },
 
   created() {
     console.log('this is me? ', this.isMe ? 'yes!' : 'no!');
+
+    if (!this.isMe) {
+      this.pending = true;
+      this.getUser().then(() => {
+        this.pending = false;
+      });
+    }
   },
+
 };
