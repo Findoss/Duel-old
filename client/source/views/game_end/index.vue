68465853ac17ce169b62ef3906329ed0f4229e3a
Nikita Stroganov <findoss@yandex.ru>
2018-12-27 14:47:13 +0300

    add view Game end

---
+++ client/source/views/game_end/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <z-container-center>
+    <div class="game-end">
+      game-end
+      <z-button>
+        <router-link :to="{ name: 'profile', params: { userId: this.$store.getters.myId, force: true } }">
+          profile
+        </router-link>
+      </z-button>
+      <z-button>
+        <router-link :to="{ name: 'lobby', params: { force: true } }">
+          play more
+        </router-link>
+      </z-button>
+    </div>
+  </z-container-center>
+</template>
+<script src="./script.js"></script>
+<style src="./style.css" scoped></style>
