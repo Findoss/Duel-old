af276cef479c26f96051160901ef8192b173e8eb
Nikita Stroganov <findoss@yandex.ru>
2018-12-28 19:37:46 +0300

    update view game

--- client/source/views/game/index.vue
+++ client/source/views/game/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="game">
-    <div class="a1">hp me</div>
-    <div class="a2">timer</div>
-    <div class="a3">hp ooponent</div>
-    <div class="a4">me avatar</div>
-    <div class="a5">board
+    <z-user-parameters
+      class="a11"
+      :nickname="me.nickname"
+    />
 
-      <z-title :size="1">
+    <div class="a2">
+      timer
+      <z-title :size="3">
         gameId: {{ this.$route.params.gameId }}
       </z-title>
       <z-button>
@@ -15,9 +16,40 @@
         </router-link>
       </z-button>
     </div>
-    <div class="a6">opponent avatar</div>
-    <div class="a7">me panel</div>
-    <div class="a8">opponent panel</div>
+
+    <z-user-parameters
+      class="a33"
+      :nickname="opponent.nickname"
+    />
+
+    <z-user-avatar
+      class="a4"
+      :avatar="me.avatar"
+    />
+
+    <div class="a5">
+      <z-container-board>
+
+        <template v-for="(row, indexaa) in this.$store.state.game.board">
+          <div
+            v-for="(rune, index) in row"
+            :key="String(indexaa +1) + String(index +1)"
+            :class="['rune', `rune_type_${rune}` ]"
+          >
+            {{ rune }}
+          </div>
+        </template>
+
+      </z-container-board>
+    </div>
+
+    <z-user-avatar
+      class="a6"
+      :avatar="opponent.avatar"
+    />
+
+    <z-user-panel class="a7" />
+    <z-user-panel class="a8" />
   </div>
 </template>
 <script src="./script.js"></script>
--- client/source/views/game/script.js
+++ client/source/views/game/script.js
@@ -1,4 +1,31 @@
+import ContainerBoard from '@/containers/board/index.vue';
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
+
+  components: {
+    'z-container-board': ContainerBoard,
+  },
+
+
+  computed: {
+    ...mapGetters([
+      // 'myId',
+      // 'isLogin',
+    ]),
+    ...mapGetters({
+      me: 'me/getAllData',
+      opponent: 'opponent/getAllData',
+    }),
+  },
+
+  methods: {
+    // ...mapActions({
+    //   getMe: 'me/loadMe',
+    //   getUser: 'opponent/loadUser',
+    // }),
+  },
+
   beforeRouteLeave(to, from, next) {
     if (to.params.force) next();
     next(false);
