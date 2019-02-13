1f3a7c3ba595c025e242e1b378724869c8cc6896
Nikita Stroganov <findoss@yandex.ru>
2019-02-13 19:24:52 +0300

    fix typo

--- server/game/controllers/lobby.js
+++ server/game/controllers/lobby.js
@@ -14,9 +14,11 @@ module.exports.del = async (ctx) => {
   lobby.deleteUser(userId);
 
   users[userId].socket.emit('LobbyExit', 'exit');
+
+  // debug
   debug.chat(users[userId].socket, 'delete you lobby');
 
-  return 'Exit';
+  return '';
 };
 
 /**
