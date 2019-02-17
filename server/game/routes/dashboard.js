f74696b3fef17ca76314b826422ccd0f0e78786e
Nikita Stroganov <findoss@yandex.ru>
2019-02-18 01:07:01 +0300

    fix typo

--- server/game/routes/dashboard.js
+++ server/game/routes/dashboard.js
@@ -4,7 +4,7 @@ const ctrlDashboard = require('../controllers/dashboard');
 module.exports = ctx => new Router(ctx)
   .on('lobby-users', ctrlDashboard.usersLobby)
   .on('lobby-count', ctrlDashboard.usersLobbyCount)
-  .on('game-count', ctrlDashboard.count)
+  .on('game-count', ctrlDashboard.gamesCount)
   .on('world-users-count', ctrlDashboard.usersCount)
   .on('world-users', ctrlDashboard.users)
   .start();
