598ed1a6b6654b08b735ddedbec65d56ed168564
Nikita Stroganov <findoss@yandex.ru>
2019-02-20 15:21:20 +0300

    add timer step

--- client/source/store/modules/game/mutations.js
+++ client/source/store/modules/game/mutations.js
@@ -3,6 +3,24 @@
 export default {
   START_GAME: (state, payload) => {
     state.board = payload.newBoard;
+    state.stepTime = payload.stepTime;
     state.stepUser = payload.step;
+    state.currentStepTime = payload.currentStepTime;
+  },
+
+  SET_STEP_USER: (state, payload) => {
+    state.stepUser = payload.currentStepUserId;
+  },
+
+  DEC_CURRENT_TIME_STEP: (state) => {
+    state.currentStepTime -= 1000;
+  },
+
+  RESET_CURRENT_TIME_STEP: (state) => {
+    state.currentStepTime = state.stepTime;
+  },
+
+  SET_ID_TIMER_STEP: (state, idStepTimer) => {
+    state.idStepTimer = idStepTimer;
   },
 };
