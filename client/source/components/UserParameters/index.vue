5f4ebbc2d11a3a7f5acb7bc0e76609b1d38de534
Nikita Stroganov <findoss@yandex.ru>
2019-02-20 15:20:28 +0300

    update user parametrs

--- client/source/components/UserParameters/index.vue
+++ client/source/components/UserParameters/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="user-parameters">
-    <div class="a1">{{ nickname }}</div>
-    <div class="a2">99999 / 99999</div>
-    <div class="a3">
-      <div class="b1">9999</div>
-      <div class="b2">9999</div>
-      <div class="b3">9999</div>
+    <div class="user-parameters_nickname">{{ user.nickname }}</div>
+    <div class="user-parameters_hp">{{user.game.health.value}} / {{user.game.health.limit}}</div>
+    <div class="user-parameters_resources">
+      <div class="resources_energy-1">{{user.game.resources.energy_1}}</div>
+      <div class="resources_energy-2">{{user.game.resources.energy_2}}</div>
+      <div class="resources_energy-3">{{user.game.resources.energy_3}}</div>
     </div>
   </div>
 </template>
